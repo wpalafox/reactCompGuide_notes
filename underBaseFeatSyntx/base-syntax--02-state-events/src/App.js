7ef8a0b75afd49054b2c3c583aeba3a08723095d
wpalafox <wpalafox@gmail.com>
2019-05-04 13:38:05 -0500

    Passing probs

--- underBaseFeatSyntx/base-syntax--02-state-events/src/App.js
+++ underBaseFeatSyntx/base-syntax--02-state-events/src/App.js
@@ -40,23 +40,25 @@ class App extends Component {
 
 
   }
-/*
-  nameChangedHandler = (event) => {
-    this.setState( {
-      persons: [
-        { name: 'Max', age: 28 },
-        { name: event.target.value, age: 29 },
-        { name: 'Stephanie', age: 26 }
-      ]
-    } )
-  }
-*/
+
   render () {
+    const style = {
+      backgroundColor: 'white',
+      font: 'inherit',
+      border: '1px solid blue',
+      padding: '8px',
+      cursor: 'pointer'
+  };
+
+
+
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
         <p>This is really working!</p>
-        <button onClick={() => this.switchNameHandler("Steve")}>Switch Name</button>
+        <button 
+          style={style}
+          onClick={() => this.switchNameHandler("Steve")}>Switch Name</button>
         <Person 
           name={this.state.persons[0].name} 
           age={this.state.persons[0].age} />
---
+++ underBaseFeatSyntx/base-syntax--assignment-problem/src/User/UserOutput.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+
+export default class UserOutput extends Component {
+  render() {
+      console.log(this.props)
+    return (
+      <div>
+        <h1>Username: {this.props.username} </h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+          Sit amet tellus cras adipiscing enim eu turpis. Tincidunt ornare massa eget egestas purus viverra accumsan in. Semper risus 
+          in hendrerit gravida rutrum quisque non tellus. Cum sociis natoque penatibus et. Nisi lacus sed viverra tellus in hac habitasse 
+          platea. Massa ultricies mi quis hendrerit dolor magna. Euismod in pellentesque massa placerat. Purus in massa tempor nec feugiat 
+          nisl pretium. Risus ultricies tristique nulla aliquet enim tortor at auctor.</p>
+      </div>
+    )
+  }
+}
+
