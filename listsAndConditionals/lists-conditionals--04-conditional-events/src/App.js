7dcc27a7e313f7b9eb1fc03135183728ac970a92
wpalafox <wpalafox@gmail.com>
2019-05-20 07:55:19 -0500

    Handling Dynamic Content 'The Javascript Way'

--- listsAndConditionals/lists-conditionals--04-conditional-events/src/App.js
+++ listsAndConditionals/lists-conditionals--04-conditional-events/src/App.js
@@ -55,17 +55,11 @@ class App extends Component {
       cursor: 'pointer'
   };
 
+  let persons = null;
 
-
-    return (
-      <div className="App">
-        <h1>Hi, I'm a React App</h1>
-        <p>This is really working!</p>
-        <button 
-          style={style}
-          onClick={this.togglePersonsHandler}>Switch Name</button>
-        { this.state.showPersons === true ? 
-        <div >
+  if (this.state.showPersons) {
+    persons = (
+      <div >
             <Person 
               name={this.state.persons[0].name} 
               age={this.state.persons[0].age} />
@@ -78,9 +72,22 @@ class App extends Component {
               name={this.state.persons[2].name} 
               age={this.state.persons[2].age} />
 
-        </div> : null
+        </div> 
+
+    );
+
+  }
+
+
+    return (
+      <div className="App">
+        <h1>Hi, I'm a React App</h1>
+        <p>This is really working!</p>
+        <button 
+          style={style}
+          onClick={this.togglePersonsHandler}>Toggle Persons</button>
+          {persons}
         
-      }
       
       </div>
     );
