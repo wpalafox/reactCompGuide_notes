703b1ad45c0b615a9bc9bbbcf705f51e5921375d
wpalafox <wpalafox@gmail.com>
2019-05-20 21:20:07 -0500

    starting delete person handler

--- listsAndConditionals/lists-conditionals--04-conditional-events/src/App.js
+++ listsAndConditionals/lists-conditionals--04-conditional-events/src/App.js
@@ -13,16 +13,10 @@ class App extends Component {
     showPersons: false
   }
 
-  switchNameHandler = (newName) => {
-    // console.log('Was clicked!');
-    // DON'T DO THIS: this.state.persons[0].name = 'Maximilian';
-    this.setState( {
-      persons: [
-        { name: newName, age: 28 },
-        { name: 'Michael', age: 29 },
-        { name: 'Scarlett', age: 27 }
-      ]
-    } )
+  deletePersonHandler = (personIndex) => {
+    const persons = this.state.persons;
+    persons.splice(personIndex, 1); 
+    this.setState({persons: persons})
   }
 
   nameChangedHandler = (event) => {
@@ -60,17 +54,13 @@ class App extends Component {
   if (this.state.showPersons) {
     persons = (
       <div >
-            <Person 
-              name={this.state.persons[0].name} 
-              age={this.state.persons[0].age} />
-            <Person 
-              name={this.state.persons[1].name} 
-              age={this.state.persons[1].age}
-              click={this.switchNameHandler.bind(this, 'Max!')}
-              changed={this.nameChangedHandler} >My Hobbies: Racing</Person>
-            <Person 
-              name={this.state.persons[2].name} 
-              age={this.state.persons[2].age} />
+        {this.state.persons.map((person, index) => {
+          return <Person
+          click={() => this.deletePersonHandler(index)} 
+          name={person.name} 
+          age={person.age} />
+        })}
+            
 
         </div> 
 
