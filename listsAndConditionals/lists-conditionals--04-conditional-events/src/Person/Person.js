962c9dfb94f647ec4cecc0365df90d26db2c71ca
wpalafox <wpalafox@gmail.com>
2019-05-23 07:37:21 -0500

    Rendering textbox length to page

--- listsAndConditionals/lists-conditionals--04-conditional-events/src/Person/Person.js
+++ listsAndConditionals/lists-conditionals--04-conditional-events/src/Person/Person.js
@@ -8,6 +8,7 @@ const person = ( props ) => {
             <p onClick={props.click}>I'm {props.name} and I am {props.age} years old!</p>
             <p>{props.children}</p>
             <input type="text" onChange={props.changed} value={props.name}/>
+            <p>Length of text: </p>
         </div>
     )
 };
