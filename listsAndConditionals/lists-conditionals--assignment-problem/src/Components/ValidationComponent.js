2bd20a56703f42af3cbd062ce025d6129abe4f1e
wpalafox <wpalafox@gmail.com>
2019-05-25 14:54:03 -0500

    styled components

--- listsAndConditionals/lists-conditionals--assignment-problem/src/Components/ValidationComponent.js
+++ listsAndConditionals/lists-conditionals--assignment-problem/src/Components/ValidationComponent.js
@@ -1,14 +1,21 @@
 import React, { Component } from 'react'
 
 export default class ValidationComponent extends Component {
+    
+    
     render() {
+        
+        
+        
+        
         return (
             <div>
                 <h1>{this.props.textL}</h1>
-                {/*This part will require conditional rendering :)*/}
-                
-                <p>Text Length: </p>
-
+               
+            { this.props.validState? 
+                <h2>Validation: Long Enough </h2>
+                :  <h2>Validation: Too short </h2>
+            }
             </div>
         )
     }
